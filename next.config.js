505dff2fe14f89c3a567a59f720f5f933c6ea46d
bmanss <blake_a_tucker@live.com>
2023-12-21 10:30:56 -0500

    commit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-          {
-            source: '/mojang/uuid/:slug*',
-            destination: 'https://api.mojang.com/users/profiles/minecraft/:splat',
-            permanent: true,
-          },
-        ]
-      },
+
 }
 
 module.exports = nextConfig
