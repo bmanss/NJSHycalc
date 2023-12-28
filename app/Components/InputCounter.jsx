5e9ff877369b7efbc811cc5562c4b0b398c7f6ad
bmanss <blake_a_tucker@live.com>
2023-12-28 17:07:05 -0500

    option for static only values (can't change)

--- app/Components/InputCounter.jsx
+++ app/Components/InputCounter.jsx
@@ -1,21 +1,21 @@
-'use client'
+"use client";
 import React, { useEffect } from "react";
 import { useState } from "react";
-import Image from 'next/image';
-import styles from '../styles/InputCounter.module.scss'
+import Image from "next/image";
+import styles from "../styles/InputCounter.module.scss";
 
-export const InputCounter = ({ value, onChange, min, max, inputWidth, floating, precision, step }) => {
+export const InputCounter = ({ value, onChange, min, max, inputWidth, floating, precision, step, isStatic }) => {
   const [counterValue, setCounterValue] = useState(() => {
     const val = value ?? 0;
     return Number(val).toFixed(precision ?? 0);
   });
 
   // if value manually changed update it
-  useEffect(() =>{
-    if (value !== counterValue){
+  useEffect(() => {
+    if (value !== counterValue) {
       setCounterValue(Number(value).toFixed(precision ?? 0));
     }
-  },[value])
+  }, [value]);
 
   function updateValue(newVal) {
     let val = Math.max(min ?? 0, Math.min(max ?? 0, newVal));
@@ -34,20 +34,23 @@ export const InputCounter = ({ value, onChange, min, max, inputWidth, floating, 
   }
 
   return (
-    <span className={styles['inputCounter']}>
+    <span className={styles["inputCounter"]}>
       <input
         style={{ width: inputWidth && inputWidth }}
+        readOnly={isStatic ? true : false}
         value={counterValue}
         onChange={(e) => handleSetCounter(e.target.value)}
         onBlur={(e) => updateValue(e.target.value)}></input>
-      <span style={{ display: "flex", flexDirection: "column" }}>
-        <button onClick={() => updateValue(parseFloat(counterValue) + (step || 1))}>
-          <Image width={15} height={15} src='/arrowUp.png' alt='arrowUp' />
-        </button>
-        <button onClick={() => updateValue(parseFloat(counterValue) - (step || 1))}>
-          <Image width={15} height={15} src='/arrowDown.png' alt='arrowDown' />
-        </button>
-      </span>
+      {!isStatic && (
+        <span style={{ display: "flex", flexDirection: "column" }}>
+          <button onClick={() => updateValue(parseFloat(counterValue) + (step || 1))}>
+            <Image width={15} height={15} src='/arrowUp.png' alt='arrowUp' />
+          </button>
+          <button onClick={() => updateValue(parseFloat(counterValue) - (step || 1))}>
+            <Image width={15} height={15} src='/arrowDown.png' alt='arrowDown' />
+          </button>
+        </span>
+      )}
     </span>
   );
 };
