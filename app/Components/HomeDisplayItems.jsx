46e23b3e9f333a1ccba506acbfd7a308cb8da22d
bmanss <blake_a_tucker@live.com>
2023-12-29 00:48:29 -0500

    display weapon

--- app/Components/HomeDisplayItems.jsx
+++ app/Components/HomeDisplayItems.jsx
@@ -21,9 +21,10 @@ const HomeDisplayItems = () => {
   const displayItem = {
     ...profileContext.getGearPiece("helmet"),
     name: "Superior Dragon Helmet",
-    id: "SUPERIOR_DRAGON_HELMET",
+    id: "none",
     itemType: "dungeon",
     tier: "LEGENDARY",
+    modifier: 'ancient',
     starLevel: 10,
     hotPotatoCount: 7,
     nonDungeonStats: {
@@ -46,6 +47,24 @@ const HomeDisplayItems = () => {
       },
     ],
   };
+  const displayWeapon = {
+    ...profileContext.getGearPiece("weapon"),
+    name: "Shadow Fury",
+    id: "none",
+    itemType: "dungeon",
+    tier: "LEGENDARY",
+    category: "weapon",
+    generalCategory:"sword",
+    starLevel: 5,
+    hotPotatoCount: 0,
+    referenceCategory: 'sword',
+    nonDungeonStats: {
+        WALK_SPEED: 40,
+        DAMAGE: 310,
+        STRENGTH: 160,
+        CRITICAL_DAMAGE: 60
+    },
+  };
 
   return (
     <div className={styles.displayGear}>
@@ -55,6 +74,13 @@ const HomeDisplayItems = () => {
         gearPiece={displayItem}
         reforgeList={reforgeList.helmet}
         enchantmentList={enchantmentList.helmet}
+      />
+      <ItemCard
+        itemList={null}
+        displayOnly={true}
+        gearPiece={displayWeapon}
+        reforgeList={reforgeList.sword}
+        enchantmentList={enchantmentList.sword}
       />
       <PetDisplay displayOnlyPet={displayPet} />
     </div>
