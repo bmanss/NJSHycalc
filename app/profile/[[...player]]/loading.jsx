e267f56bcbe07fc8e4c2f8218d07e344bdc1cf22
bmanss <blake_a_tucker@live.com>
2023-12-31 21:16:09 -0500

    added loading skeleton component

--- app/profile/[[...player]]/loading.jsx
+++ app/profile/[[...player]]/loading.jsx
@@ -1,42 +1,8 @@
 import React from "react";
-import LoadingStyles from "../../styles/loading.module.scss";
-import StatBarStyles from "../../styles/StatDisplays.module.scss";
-import ItemCardStyles from "../../styles/ItemCard.module.scss";
-import InfoBarStyles from "../../styles/InfoBar.module.scss"
+import LoadingSkeleton from "../../Components/LoadingSkeleton";
+
 const loading = () => {
-  return (
-    <div style={{ height: "100vh", color: "white" }}>
-      <div className={InfoBarStyles["InfoBar"]}>
-        <div className={InfoBarStyles["InfoBar-PlayerInfo"]}>Loading Player</div>
-      </div>
-      <div style={{ display: "flex", height: "100%" }}>
-        <div className={StatBarStyles["StatsBar"]}>
-          <div className={StatBarStyles["StatsBar-Header"]}>Stats:</div>
-          <div className={LoadingStyles.LoadingBlock}></div>
-          <div className={StatBarStyles["StatsBar-Header"]} style={{ marginTop: "10px" }}>
-            Damage Stats:
-          </div>
-          <div className={LoadingStyles.LoadingBlock}></div>
-        </div>
-        <div className='ContentContainer'>
-          <div className='ContentNav'>
-            <span className={`ContentNav-Option`}>Stat Extras</span>
-            <span className={`ContentNav-Option`}>Skills</span>
-            <span className={`ContentNav-Option`}>Collections</span>
-            <span className={`ContentNav-Option active`}>Armor</span>
-            <span className={`ContentNav-Option `}>Equipment</span>
-            <span className={`ContentNav-Option `}>Combat Gear</span>
-          </div>
-          <div className={ItemCardStyles["ItemCard-Container"]}>
-            <div className={LoadingStyles.LoadingBlock}></div>
-            <div className={LoadingStyles.LoadingBlock}></div>
-            <div className={LoadingStyles.LoadingBlock}></div>
-            <div className={LoadingStyles.LoadingBlock}></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <LoadingSkeleton />;
 };
 
 export default loading;
