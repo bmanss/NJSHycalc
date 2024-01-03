053208fde96b25516de18941832af55081b95395
bmanss <blake_a_tucker@live.com>
2024-01-02 22:07:24 -0500

    methods for getting collection data

--- app/lib/DatabaseMethods.js
+++ app/lib/DatabaseMethods.js
@@ -1,4 +1,4 @@
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import { doc, getDoc, getDocs, setDoc, collection } from "firebase/firestore";
 
 export async function fetchProfile(firestore, UUID) {
   const profileDocRef = doc(firestore, "profileData", UUID);
@@ -18,4 +18,12 @@ export function setProfile(firestore, UUID, data) {
 
 export function setProfileWithAdmin(adminFirestore, UUID, data) {
     adminFirestore.collection("profileData").doc(UUID).set(data);
+}
+
+export async function getCollection(firestoreDB,collectionName){
+    return await getDocs(collection(firestoreDB, collectionName));
+}
+
+export async function getCollectionWithAdmin(firestoreDB,collectionName){
+    return await firestoreDB.collection(collectionName).get();
 }
