934dc2d14cc8d7d2ec02666edf25e8fcaf994636
bmanss <blake_a_tucker@live.com>
2024-01-08 00:13:10 -0500

    param for max value

--- app/Components/EffectInput.jsx
+++ app/Components/EffectInput.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import InputCounter from './InputCounter'
 import InputStyles from '../styles/InputCounter.module.scss'
-const EffectInput = ({text, value, onChange}) => {
+const EffectInput = ({max, text, value, onChange}) => {
   return (
     <div className={InputStyles.EffectInput} style={{fontSize:'12px',display:'flex',alignItems:'center'}}>
         <span>{text || ''}</span>
-        <InputCounter inputWidth={'100%'} min={0} max={100000000000} value={value} onChange={onChange}  />
+        <InputCounter inputWidth={'100%'} min={0} max={max || 100000000000} value={value} onChange={onChange}  />
     </div>
   )
 }
