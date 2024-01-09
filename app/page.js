3420e36c9a3751a55e702f9f593223302bdd54f5
bmanss <blake_a_tucker@live.com>
2024-01-08 22:25:47 -0500

    test

--- app/page.js
+++ app/page.js
@@ -1,14 +1,38 @@
+'use client'
 import styles from "./styles/homePage.module.scss";
 import { ProfileProvider } from "./context/ProfileContext";
 import HomeDisplayItems from "./Components/HomeDisplayItems";
-import HomeSearchForm from "./Components/HomeSearchForm";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef } from "react";
+import { updateDatabaseItems } from "./LocalTesting/updateDatabaseItems";
 
-// updateDatabaseItems();
-export default async function Home() {
+
+export default function Home() {
+  // updateDatabaseItems();
+  const router = useRouter();
+  const playerName = useRef(''); 
+  const loadProfile = (e) =>{
+    e.preventDefault();
+    router.push(`/profile/${playerName.current}`);
+  }
+
+  const loadDefault = async (e) =>{
+    e.preventDefault();
+    router.push(`/profile/`);
+  }
+
+  useEffect(() =>{
+    router.prefetch(`/profile`);
+  },[router])
+
   return (
     <main className={styles.main}>
       <h1 className={styles.headerText}>HyCalc</h1>
-      <HomeSearchForm />
+      <form className={styles.profileSearch} onSubmit={(e) => loadProfile(e)}>
+        <input onChange={(e) => playerName.current = e.target.value} placeholder='Player Name' type='text' />
+        <button>Load</button>
+        <button onClick={(e) =>loadDefault(e)}>No Profile</button>
+      </form>
       <div>
         <ProfileProvider>
           <HomeDisplayItems />
