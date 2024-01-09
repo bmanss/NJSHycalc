9a5fad998c27bd50f47dd49a4c5d19f1a9f15964
bmanss <blake_a_tucker@live.com>
2024-01-08 23:57:25 -0500

    loading screen on route

--- app/page.js
+++ app/page.js
@@ -3,21 +3,27 @@ import styles from "./styles/homePage.module.scss";
 import { ProfileProvider } from "./context/ProfileContext";
 import HomeDisplayItems from "./Components/HomeDisplayItems";
 import { useRouter } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { updateDatabaseItems } from "./LocalTesting/updateDatabaseItems";
+import LoadingSkeleton from "./Components/LoadingSkeleton"
 
-
-export default function Home() {
   // updateDatabaseItems();
+export default function Home() {
   const router = useRouter();
   const playerName = useRef(''); 
+
+  // use loading screen here not sure why loading page does not come into effect on route
+  const [loading,setLoading] = useState(false);
+
   const loadProfile = (e) =>{
     e.preventDefault();
+    setLoading(true);
     router.push(`/profile/${playerName.current}`);
   }
 
   const loadDefault = async (e) =>{
     e.preventDefault();
+    setLoading(true);
     router.push(`/profile/`);
   }
 
@@ -25,19 +31,25 @@ export default function Home() {
     router.prefetch(`/profile`);
   },[router])
 
-  return (
-    <main className={styles.main}>
-      <h1 className={styles.headerText}>HyCalc</h1>
-      <form className={styles.profileSearch} onSubmit={(e) => loadProfile(e)}>
-        <input onChange={(e) => playerName.current = e.target.value} placeholder='Player Name' type='text' />
-        <button>Load</button>
-        <button onClick={(e) =>loadDefault(e)}>No Profile</button>
-      </form>
-      <div>
-        <ProfileProvider>
-          <HomeDisplayItems />
-        </ProfileProvider>
-      </div>
-    </main>
-  );
+  if (loading){
+    return <LoadingSkeleton/>
+  }
+  else {
+    return (
+      <main className={styles.main}>
+        <h1 className={styles.headerText}>HyCalc</h1>
+        <form className={styles.profileSearch} onSubmit={(e) => loadProfile(e)}>
+          <input onChange={(e) => playerName.current = e.target.value} placeholder='Player Name' type='text' />
+          <button>Load</button>
+          <button onClick={(e) =>loadDefault(e)}>No Profile</button>
+        </form>
+        <div>
+          <ProfileProvider>
+            <HomeDisplayItems />
+          </ProfileProvider>
+        </div>
+      </main>
+    );
+  }
+
 }
