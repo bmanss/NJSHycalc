820e52b291c75f9f6267639c6ebe3dbd97f82a21
bmanss <blake_a_tucker@live.com>
2024-01-12 17:38:49 -0500

    title,buttons style changes

--- app/page.js
+++ app/page.js
@@ -1,51 +1,50 @@
-'use client'
+"use client";
 import styles from "./styles/homePage.module.scss";
 import { ProfileProvider } from "./context/ProfileContext";
 import HomeDisplayItems from "./Components/HomeDisplayItems";
 import { useRouter } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 import { updateDatabaseItems } from "./LocalTesting/updateDatabaseItems";
-import LoadingSkeleton from "./Components/LoadingSkeleton"
+import LoadingSkeleton from "./Components/LoadingSkeleton";
 
-  // updateDatabaseItems();
+// updateDatabaseItems();
 export default function Home() {
   const router = useRouter();
-  const playerName = useRef(''); 
+  const playerName = useRef("");
 
   // use loading screen here not sure why loading page does not come into effect on route
-  const [loading,setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const loadProfile = (e) =>{
+  const loadProfile = (e) => {
     e.preventDefault();
     setLoading(true);
     router.push(`/profile/${playerName.current}`);
-  }
+  };
 
-  const loadDefault = async (e) =>{
+  const loadDefault = async (e) => {
     e.preventDefault();
     setLoading(true);
     router.push(`/profile/`);
-  }
+  };
 
-  useEffect(() =>{
+  useEffect(() => {
     setLoading(false);
-  },[]);
+  }, []);
 
-  useEffect(() =>{
+  useEffect(() => {
     router.prefetch(`/profile`);
-  },[router])
+  }, [router]);
 
-  if (loading){
-    return <LoadingSkeleton/>
-  }
-  else {
+  if (loading) {
+    return <LoadingSkeleton />;
+  } else {
     return (
       <main className={styles.main}>
         <h1 className={styles.headerText}>HyCalc</h1>
         <form className={styles.profileSearch} onSubmit={(e) => loadProfile(e)}>
-          <input onChange={(e) => playerName.current = e.target.value} placeholder='Player Name' type='text' />
-          <button>Load</button>
-          <button onClick={(e) =>loadDefault(e)}>No Profile</button>
+          <input onChange={(e) => (playerName.current = e.target.value)} placeholder='Player Name' type='text' />
+          <button className={styles.HomeButton}><span>Load</span></button>
+          <button className={styles.HomeButton} onClick={(e) => loadDefault(e)}><span>No Profile</span></button>
         </form>
         <div>
           <ProfileProvider>
@@ -55,5 +54,4 @@ export default function Home() {
       </main>
     );
   }
-
 }
